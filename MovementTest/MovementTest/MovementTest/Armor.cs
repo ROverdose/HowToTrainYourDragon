ba1cebd618422ba51228bd5c018f80e518321d9f
ROverdose <dscarrel@gmail.com>
2015-04-29 00:07:44 -0400

    Item Sheet started
    Going to need to build the naming system a bit more.

---
+++ MovementTest/MovementTest/MovementTest/Armor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovementTest
+{
+    class Armor : ItemSheet
+    {
+        public Armor()
+        {
+        }
+
+        //TODO: I will make a random affix generator object that will grant these objects their stats
+    }
+}
---
+++ MovementTest/MovementTest/MovementTest/ItemSheet.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovementTest
+{
+    class ItemSheet
+    {
+        private int buyCost,
+            sellCost;
+        private string itemName;
+        private bool equippable;
+
+        public int Buy_Cost
+        {
+            get { return buyCost; }
+            set { buyCost = value; }
+        }
+        public int Sell_Cost
+        {
+            get { return sellCost; }
+            set { sellCost = value; }
+        }
+        public string Item_Name
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+        public bool Can_Equip
+        {
+            get { return equippable; }
+            set { equippable = value; }
+        }
+        
+
+    }
+    public class AffixGenerator
+    {
+        //Refer to Weapon stuff reference for the effects of these prefixes
+        public enum Prefix
+        {
+            Angelic,
+            Arcane,
+            Bloodthirsty,
+            Broken,
+            Brutal,
+            Cosmic,
+            Creeping,
+            Demonic,
+            Dwarven,
+            Empowered,
+            Enchanted,
+            Enfeebling,
+            Eternal,
+            Fine,
+            Flaming,
+            Freezing,
+            Gem_Encrusted,
+            Gleaming,
+            Golden,
+            Guardsman,
+            Heavy,
+            Holy,
+            Lethal,
+            Light,
+            Macabre,
+            Charging,
+            Perfect,
+            Pestiliential,
+            Rogue,
+            Runic,
+            Serrated,
+            Sharp,
+            Unholy,
+            Vorpal,
+            Wooden,
+            Worn
+        }
+    }
+}
